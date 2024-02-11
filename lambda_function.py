b496c1baa1814bdeaaabf9fadb91bce2111165c2
SaharShelach <saharshel@gmail.com>
2024-02-11 11:09:54 +0200

    add python code and github actions workflow

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import json
+
+def lambda_handler(event, context):
+    name = event.get('queryStringParameters', {}).get('name')
+    if name is not None:
+        message = f'Hello {name}!'
+    else:
+        message = 'Please provide a name parameter in the query string.'
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({'message': message})
+    }
+
+    return response
